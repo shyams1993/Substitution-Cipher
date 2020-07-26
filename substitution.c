ef48872cb482cdcafa16bc3beecac4edc9cc119e
Shyam Salil <shyamsalil1993@gmail.com>
2020-07-26 21:32:41 +0530

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -19,20 +19,24 @@ int main(int argc, string argv[])
             /* If and only if the length of the key is 26, the program begins checking if the string has duplicate characters or numbers in it */
             string key = argv[1];
             int counter1 = 0, counter2 = 0;
+            bool seen[150] = {false};
             for (int i = 0; i < length; i++)
             {
                 /* Exception Check #3 - To check if the letter is an alphabet and that no alphabet repeats twice */
                 /* A for loop to iterate over each character to check if each character in the string is an alphabet; If not, increments a counter variable */
                 if (isalpha(key[i]))
                 {
-                    /* Logic to check for repeating characters by using the first loop & introducing another loop that checks all the characters in the string 
-                    If the characters are same, then the counter increments else, the counter remains zero */
-                    for (int j = i + 1; j < length; j++)
+                    /* Logic to check for repeating characters by declaring a boolean array initialized to false */
+                    /* Through the traversal, key[i] is used to reference the boolean array's index and checks if that index is marked as True
+                    If that index has been marked as true, then a counter is incremented. If the index has not been marked as true, then we mark it as true */
+                    int c = key[i];
+                    if (seen[c] == true)
                     {
-                        if (key[i] == key[j])
-                        {
-                            counter1++;
-                        }
+                        counter1++;
+                    }
+                    else
+                    {
+                        seen[c] = true;
                     }
                 }
                 else
